38fde67d2e4d512a7f0f4003338364ffe2c247bb
Gregory Smetana <smetangs@gmail.com>
2016-11-20 19:36:11 -0800

    first attempt at reducing cache

---
+++ travis/travis_clean_pv_cache.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# remove cmake files so travis doesn't upload entire cache every time
+
+PV_DIR=/home/smetana/Documents/ParaView-bin
+
+shopt -s nullglob
+
+rm -fv $PV_DIR/CMakeFiles/CMakeError.log
+rm -fv $PV_DIR/CMakeFiles/Makefile2
+rm -fv $PV_DIR/CMakeFiles/Makefile.cmake
+
+
+for f in $PV_DIR/lib/cmake/paraview-$PV_VERSION/Modules/*.cmake
+do
+  rm -fv $f
+done
